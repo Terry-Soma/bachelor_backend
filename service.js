3f5f530170b049edaa16a021a5e182bf736c0500
Terry-Soma <jackhack116@yahoo.com>
2023-06-02 15:21:01 +0800

    bagshid uzuulne

--- service.js
+++ service.js
@@ -53,9 +53,10 @@ const whitelist = [
 // };
 const corsOptions = {
   origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 }
+
 app.use(cors())
 // app.use(helmet());
 // app.use(cors(corsOptions));
