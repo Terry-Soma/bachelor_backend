d5f60cd45f9c8b94cb55e2e68a0e87c43f7091cf
Terry-Soma <jackhack116@yahoo.com>
2023-06-08 01:43:01 +0800

    sql

---
+++ needsql.sql
@@ -0,0 +1,29 @@
+﻿# toolood eremblene
+SELECT value, COUNT(*) AS count
+FROM eburtgel
+GROUP BY value
+ORDER BY s.id DESC;
+
+
+# surguuli bolon too checked
+SELECT count(s.id) as too,s.name
+FROM eburtgel e
+inner join mergejil m
+on m.id = e.mergejilId
+inner join hutulbur h
+on h.id = m.hutulburId
+inner join school s
+on s.id = h.schoolId
+group by s.id
+order by too DESC;
+
+
+
+#2
+select count(burtgel_Id) as niit_elsegch
+from elsegch;
+
+#1
+select count(id) as niit_mergejil_songolt
+from eburtgel;
+
